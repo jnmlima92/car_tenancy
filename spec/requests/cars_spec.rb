5ca27ed0787a324e8f6d05ecb38e205e02968d57
joao.moura <jnmlima92@gmail.com>
2020-10-01 00:37:11 -0300

    Fix in tests

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -9,15 +9,12 @@ RSpec.describe "Cars", type: :request do
         sign_in create :user
       end
       
-      it "works! (now write some real specs)" do
-        get cars_path
-        expect(response).to have_http_status(200)
-      end
-  
       it "returning datas from all cars" do
         cars = create_list(:car, 5)
         
         get cars_path
+        
+        expect(response).to have_http_status(200)
         expect(response.body).to include("Cars")
         
         cars.each do |car|
@@ -27,6 +24,15 @@ RSpec.describe "Cars", type: :request do
           expect(response.body).to include(car.model_year.to_s)
           expect(response.body).to include(car.plate)
         end
+      end
+    end
+
+    context "withOUT a logged user" do
+      it "redirect to login page" do
+        create_list(:car, 5)
+  
+        get cars_path
+        expect(response).to redirect_to new_user_session_path
       end
     end
   end
@@ -44,39 +50,68 @@ RSpec.describe "Cars", type: :request do
         expect(response).to have_http_status(200)
       end
     end
+
+    context "withOUT a logged user" do
+      it "redirect to login page" do
+        car = create(:car)
+  
+        get car_path car
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 
-  describe "POST /car/:id with a logged user" do
-    context "when has valid params" do
-      before(:each) do 
-        sign_in create :user
-      end
-      
-      it "create a car with right attributes" do
-        car_attr = FactoryBot.attributes_for :car
+  describe "POST /car/:id" do
+    context "with a logged user" do
+      context "when has valid params" do
+        before(:each) do 
+          sign_in create :user
+        end
+        
+        it "create a car with right attributes" do
+          car_attr = FactoryBot.attributes_for :car
 
+          post cars_path, params: { car: car_attr }
+          expect(Car.last).to have_attributes(car_attr)
+        end
+      end
+
+      context "when has invalid params" do
+        it "does not create the tenancy" do
+          # model is nil
+          car_attr = FactoryBot.attributes_for :car
+          car_attr[:model] = nil
+
+          expect { post cars_path, params: { car: car_attr } }.to_not change(Car, :count)
+        end
+      end
+    end
+  
+    context "without a logged user" do
+      it "redirect to login page" do
+        car_attr = FactoryBot.attributes_for :car
+  
         post cars_path, params: { car: car_attr }
-        expect(Car.last).to have_attributes(car_attr)
-      end
-    end
-
-    context "when has invalid params" do
-      it "does not create the tenancy" do
-        # model is nil
-        car_attr = FactoryBot.attributes_for :car
-        car_attr[:model] = nil
-
-        expect { post cars_path, params: { car: car_attr } }.to_not change(Car, :count)
+        expect(response).to redirect_to new_user_session_path
       end
     end
   end
 
-  describe "POST /car/:id without a logged user" do
-    it "redirect to login page" do
-      car_attr = FactoryBot.attributes_for :car
+  describe "GET /model without a logged user" do
+    context "with a logged user" do
+      it "redirect to login page" do
+        sign_in create :user
 
-      post cars_path, params: { car: car_attr }
-      expect(response).to redirect_to new_user_session_path
+        get models_path format: 'json'
+        expect(response).to have_http_status 200
+      end
+    end
+
+    context "without a logged user" do
+      it "redirect to login page" do
+        get models_path format: 'json'
+        expect(response).to have_http_status 401
+      end
     end
   end
 end
